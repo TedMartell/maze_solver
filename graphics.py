f5f5c433526d0f92335fa9465fb761ed1067bc1e
TedMartell <martellted@gmail.com>
2024-04-27 09:26:57 +0300

    namechanges and moved Line, Point to graphics

---
+++ graphics.py
@@ -0,0 +1,57 @@
+from tkinter import Tk, BOTH, Canvas
+
+
+class Window:
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__running = True
+        while self.__running:
+            self.redraw()
+        print("window closed...")
+
+    def close(self):
+        self.__running = False
+
+    def draw_line(self, line, fill_colour):
+        self.line = line
+        self.fill_colour = fill_colour
+        line.draw(self.__canvas, fill_colour)
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, canvas, fill_color ="red"):
+        x1 = self.point1.x
+        y1 = self.point1.y
+        x2 = self.point2.x
+        y2 = self.point2.y
+        canvas.create_line(x1, y1, x2, y2, fill=fill_color, width=2)
+
+
+
+        
+
+
+
+    
+
+        
+
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from window import Window 
+from graphics import Window, Line, Point 
+
 
 def main():
     win = Window(800, 600)  # Adjust the size as needed
+    start_point = Point(100,100)
+    end_point = Point(200,200)
+    lines = Line(start_point, end_point)
+    win.draw_line(lines, fill_colour="red")
     win.wait_for_close()
 
 if __name__ == "__main__":
