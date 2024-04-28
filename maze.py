4e182e169102fb719a533dbaef45c5e30938221e
TedMartell <martellted@gmail.com>
2024-04-28 15:39:50 +0300

    draw_maze method to maze (just for fun) and changed main to test it

--- maze.py
+++ maze.py
@@ -15,33 +15,37 @@ class Maze:
         win = None,
     ):
         
-        self.x1 = x1
-        self.y1 = y1
-        self.num_rows = num_rows
-        self.num_cols = num_cols
-        self.cell_size_x = cell_size_x
-        self.cell_size_y = cell_size_y
+        self._cells = []
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
         self._win = win
 
         self._create_cells()
 
-    def _create_cells(self):       
-        self._cells = []
-        for i in range(self.num_cols):
-            column = []
-            for j in range(self.num_rows):
-                cell = Cell(self._win)
-                column.append(cell)
+        self._create_cells()
+
+    def _create_cells(self):
+        for i in range(self._num_cols):
+            col_cells = []
+            for j in range(self._num_rows):
+                col_cells.append(Cell(self._win))
+            self._cells.append(col_cells)
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
                 self._draw_cell(i, j)
-            self._cells.append(column)
-            
-      
+
     def _draw_cell(self, i, j):
         if self._win is None:
             return
-        cell_x = self.x1 + i * self.cell_size_x
-        cell_y = self.y1 + j * self.cell_size_y
-        self._cells[i][j].draw(cell_x, cell_y + self.cell_size_y, cell_x + self.cell_size_x, cell_y)
+        x1 = self._x1 + i * self._cell_size_x
+        y1 = self._y1 + j * self._cell_size_y
+        x2 = x1 + self._cell_size_x
+        y2 = y1 + self._cell_size_y
+        self._cells[i][j].draw(x1, y1, x2, y2)
         self._animate()
 
     def _animate(self):
@@ -49,6 +53,17 @@ class Maze:
             return
         self._win.redraw()
         time.sleep(0.05)
+
+    def _break_entrance_and_exit(self):
+        pass
+
+    def draw_maze(self):
+        for i, column in enumerate(self._cells):
+            for j, cell in enumerate(column):
+            # Calculate cell corners
+                x1 = self._x1 + j * self._cell_size_x
+                y1 = self._y1 + i
+
 
 
 
