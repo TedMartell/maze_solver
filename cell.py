c17579c4e43f6c262d06cc1a839e5d7c34d170e7
TedMartell <martellted@gmail.com>
2024-04-28 07:56:41 +0300

    draw_move added to cell and new tests to main

--- cell.py
+++ cell.py
@@ -39,3 +39,19 @@ class Cell:
             end_point = Point(self._x2, self._y2)
             lines = Line(start_point, end_point)
             self._win.draw_line(lines, fill_colour="black")
+
+    def draw_move(self, to_cell, undo=False):
+        halfway_y = self._y1 + ((self._y2 - self._y1) / 2)
+        halfway_x = self._x1 + ((self._x2 - self._x1) / 2)
+        start_point = Point(halfway_x, halfway_y)
+        halfway_to_cell_y = to_cell._y1 + ((to_cell._y2 - to_cell._y1) / 2)
+        halfway_to_cell_x = to_cell._x1 + ((to_cell._x2 - to_cell._x1) / 2)
+        end_point = Point(halfway_to_cell_x, halfway_to_cell_y)
+        lines = Line(start_point, end_point)
+        if undo:
+            self._win.draw_line(lines, fill_colour="gray")
+        else:
+            self._win.draw_line(lines, fill_colour="red")
+            
+
+
